9f01d8b3e3fd26e82d1735b5947841bd8dc286de
Irina <57304442+IrinaCan@users.noreply.github.com>
2022-08-23 11:10:09 +0300

    Update taking.js

--- view/taking.js
+++ view/taking.js
@@ -1,11 +1,7 @@
-////// "раздел" для работы с выдачами книг //////////////
-///////////////////////////////////////////////////////
-
-// создание таблицы с выдачами книг
 function createTableTaking(data) {
     cleanDivMain();
     createDivMain();
-    // кнопка "Добавить новую выдачу"
+   
     var buttonCreateTaking = document.createElement("button");
     buttonCreateTaking.classList.add("buttonCreateTaking");
     buttonCreateTaking.innerHTML = "Оформить выдачу книги";
@@ -15,21 +11,20 @@ function createTableTaking(data) {
         return formCreateTaking();  // форма для создании выдачи
    
     }
-    // блок для таблицы с выдачами
+	
     var divTableTaking = document.createElement("div");
     divTableTaking.classList.add("divTableTaking");
     document.getElementById('divMain').append(divTableTaking);
 
-    // создание таблицы с выдачами    
     var tableTaking ="<table class='tableTaking' id='tableTaking' border='1'>"
-    +"<tr>"
+    	+"<tr>"
         +"<th>Номер выдачи</th>"
         +"<th>Дата выдачи</th>"
         +"<th>Читатель</th>"
         +"<th>Книга</th>"
         +"<th>Дата возврата</th>"
         +"<th>Отметить возврат</th>"
-    +"</tr>";
+    	+"</tr>";
     for(x in data) {
     tableTaking+="<tr onclick = 'changeStatus(this)'>"
         +"<td>"+data[x].id+"</td>"
@@ -42,22 +37,21 @@ function createTableTaking(data) {
         var textChange ='На руках';
         var colorTdChange = 'orangered';     
         }
-            else { 
-                var textChange='Возврат<br>'+convertDate(data[x].given);
-               var colorTdChange = 'rgb(179, 235, 95)';         
+    else { 
+    	var textChange='Возврат<br>'+convertDate(data[x].given);
+        var colorTdChange = 'rgb(179, 235, 95)';         
    } 
    tableTaking+="<td id='tdChange'class='tdChange' "
    +"style='background:"+colorTdChange+"';>"+textChange+"</td></tr>";
-}
+   }
     +"</table>";
-    divTableTaking.innerHTML = tableTaking;
+  divTableTaking.innerHTML = tableTaking;
 }
 
-// изменить статус книги - установить отметку о возврате книги
 function changeStatus(tr) {
-    var indexTaking = tr.rowIndex; // номер строки 
+    var indexTaking = tr.rowIndex; 
     var table = document.getElementById('tableTaking');
-    var today = todayDate(); // создать "сегодняшнюю" дату
+    var today = todayDate(); 
     var dataForChange = {
         method:"changeInLibr",
         idTaking:table.rows[indexTaking].cells[0].innerHTML,
@@ -66,15 +60,12 @@ function changeStatus(tr) {
         year:today.substr(6,9)
         };
     var dataForChange =  JSON.stringify(dataForChange); 
-if(confirm("Отметить возврат книги?")==true) {  
-zaprosPostAjax('../controllers/TakingController.php', dataForChange);  
-
-zaprosGetAjax('../controllers/TakingController.php'+'?'+'method=readAll', createTableTaking);    
+    if(confirm("Отметить возврат книги?")==true) {  
+	zaprosPostAjax('../controllers/TakingController.php', dataForChange);  
+	zaprosGetAjax('../controllers/TakingController.php'+'?'+'method=readAll', createTableTaking);    
+	}
 }
-}
 
-
-// создать форму для добавления новой выдачи книги
 function formCreateTaking() {
 	var divFormCreateTaking = document.createElement("div");
 	divFormCreateTaking.classList.add("divFormCreateTaking");
@@ -84,7 +75,7 @@ function formCreateTaking() {
 	var formCreateTaking = "<div id='divForm'>"
 	+"<button class='btnCloseForm' onclick= divMain.removeChild(document.getElementById('divFormCreateTaking'))>Закрыть</button>"
 	    // форма
-    +"<form id='formCreateTaking'><table>"
+   	+"<form id='formCreateTaking'><table>"
         +"<tr><td>Дата выдачи</td>"
         +"<td><input type='text' name='dateTake' value ='"+todayDate()+"' required></input></td></tr>"
     	+"<tr><td>Читатель</td>"
@@ -98,41 +89,38 @@ function formCreateTaking() {
 		+"<tr><td>Дата возврата</td>"
 		+"<td><input type='date' name='dateGive' required></input></td></tr></table>"
         +"<button type='button' class='btnCreateTaking' onclick='createTaking()'"
-    +"'>Добавить выдачу книги</button>"	
+    	+"'>Добавить выдачу книги</button>"	
 	+"</form>"
 	+"</div>";
-  
-divFormCreateTaking.innerHTML = formCreateTaking;
-makeSelectAllReaders(); // получение списка читателей
-// получение книг, доступных для выдачи (все книги минус книги "на руках")
+  divFormCreateTaking.innerHTML = formCreateTaking;
+makeSelectAllReaders(); 
 makeSelectBooksInLibr(); 
 };
 
-// получение всех читателей и вывод из в select
 function makeSelectAllReaders() {
         zaprosGetAjax('../controllers/ReaderController.php'+'?'+'method=readAll', 
     function(data) {
         for (x in data) {
-        var selectAllReaders = document.getElementById('selectAllReaders');
-        selectAllReaders.innerHTML+=
-        "<option value='"+data[x].id+"'>"+ data[x].surname+" "+data[x].name
-        +"</option>"
-        }
+        	var selectAllReaders = document.getElementById('selectAllReaders');
+        	selectAllReaders.innerHTML+=
+        	"<option value='"+data[x].id+"'>"+ data[x].surname+" "+data[x].name
+        	+"</option>"
+        	}
     })
 }
-// получение книг, доступных для выдачи (все книги минус книги "на руках")
-     function makeSelectBooksInLibr() {
+
+function makeSelectBooksInLibr() {
         zaprosGetAjax('../controllers/BookController.php'+'?'+'method=readInLibr', 
-    function(data) {
-        for (x in data) {
-        var selectBooksInLibr = document.getElementById('selectBooksInLibr');
-        selectBooksInLibr.innerHTML+=
-        "<option value='"+data[x].id+"'>"+data[x].surname+" "+data[x].name+" "
-        +data[x].lastname+"<br> \"" +data[x].title+"\"</option>"
-        }
-    })
+    		function(data) {
+        		for (x in data) {
+       			var selectBooksInLibr = document.getElementById('selectBooksInLibr');
+       			selectBooksInLibr.innerHTML+=
+        		"<option value='"+data[x].id+"'>"+data[x].surname+" "+data[x].name+" "
+       			+data[x].lastname+"<br> \"" +data[x].title+"\"</option>"
+        	}
+   	})
 }
- // отправка данных о новой выдачи
+
 function createTaking() {
     var dateTake = document.forms["formCreateTaking"]["dateTake"].value;
     var taking = {
